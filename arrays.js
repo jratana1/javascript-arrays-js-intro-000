efc79f1ead1d04378c9f645bbc998cd9fb58553e
John Ratanaprasatporn <jratana1@gmail.com>
2020-08-26 03:50:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`]
 
 function addElementToBeginningOfArray(test,element){
-  var test = ["element", ...test]
+  return (["element", ...test])
 }
